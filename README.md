63792858c2afeb69ed14f728587d9703cc992f35
Josh Bacon <bacon.josh09@gmail.com>
2019-02-24 01:16:02 -0500

    Initial commit and v1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# FRC RoboRIO Web Dashboard Standalone Application
+
+Allows for the FRC web dashboard on the National Instruments RoboRIO to be accessed from a PC with no browser that has HTML5 support. Should likely only be used in instances where a team only has their driver station laptop which is running Windows 7 or 8 and does not have any browser other than Internet Explorer installed.
---
+++ index.js
@@ -0,0 +1,141 @@
+const { app, BrowserWindow } = require('electron')
+var ping = require('ping')
+if (require('electron-squirrel-startup')) return;
+// var remote = require('remote')
+// var app = remote.require('app')
+
+// Installer things
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
+
+// Start of actual code
+var pingCfg = {
+  timeout: 10
+};
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 })
+
+  win.loadFile("connecting.html")
+
+  console.log(app.getAppPath())
+
+  var hosts = ["172.22.11.2"]
+  var validIp = false
+  hosts.forEach(function(host){
+    ping.sys.probe(host, function(isAlive){
+      var msg = isAlive ? "host " + host + " is alive" : "host " + host + " is dead"
+      console.log(msg);
+      if(isAlive){
+        console.log("Openinig web dashboard.")
+        win.loadURL(host)
+      }else{
+        win.loadFile("connectionFailed.html")
+      }
+    }, pingCfg)
+  })
+  
+  
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
